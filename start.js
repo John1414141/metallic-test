4cdfc95e8c2dc48599c33f4ee6223b9a169fd91e
Zohran14 <67291042+Zohran14@users.noreply.github.com>
2024-01-10 16:25:30 -0500

    Update start.js

--- start.js
+++ start.js
@@ -2,12 +2,13 @@
  * Example of serving static files and running a bare server.
  * This is a very common setup.
  */
-import createBareServer from "@tomphttp/bare-server-node";
-import http from "http";
-import serveStatic from "serve-static";
-import { fileURLToPath } from "url";
-import chalk from "chalk";
-import block from "./blocklist/block.json" assert { type: "json" };
+const http = require("http");
+const serveStatic = require("serve-static");
+const { fileURLToPath } = require("url");
+const chalk = require("chalk");
+const block = require("./blocklist/block.json");
+
+const createBareServer = require("@tomphttp/bare-server-node");
 
 const httpServer = http.createServer();
 const port = process.env.PORT || 8080;
