5f31efabaeba9fceebc5336dbff5fb0bce4b0ab7
Zohran14 <67291042+Zohran14@users.noreply.github.com>
2024-01-11 09:41:44 -0500

    Update app.jsx

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -29,7 +29,7 @@ function App() {
       const localPass = localStorage.getItem('metpass')
       const pass = localPass || prompt('Password: ')
 
-      if (pass == 'ab') {
+      if (pass == 'ablajdfiasljfcalcjoi1289') {
         localStorage.setItem('metpass', pass)
         setAuth(true)
       } else if (localPass) {
